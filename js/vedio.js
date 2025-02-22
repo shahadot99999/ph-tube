f97aba02d1a8b30601ee0365c18ecb1617b5e4b7
shahadot99999 <shahadot99999@gmail.com>
2025-02-22 22:52:39 +0600

    Format Time And Display Conditional Time

--- js/vedio.js
+++ js/vedio.js
@@ -4,7 +4,13 @@ console.log("vedio file are added");
 
 //create loadCategory
 
-
+function getTimeString(time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60; 
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
 
 const loadCategories = () =>{
    // console.log("loadCategories created");
@@ -65,7 +71,11 @@ const loadVidoes = () =>{
              src=${video.thumbnail}
              class = "h-full w-full object-cover"
              alt="Shoes" />
-             <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${video.others.posted_date}</span>
+             ${
+                video.others.posted_date?.length == 0 ? "": `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(video.others.posted_date)}</span>`
+
+             }
+             
         </figure>
         <div class="px-0 py-2 flex gap-2">
            <div>
--- test.js
+++ test.js
@@ -1,13 +1,22 @@
-const isVerified = "";
+// const isVerified = "";
 
-// if(isVerified == true){
-//     console.log("User is verified");
-// }else{
-//     console.log("user is not verified");
-// }
+// // if(isVerified == true){
+// //     console.log("User is verified");
+// // }else{
+// //     console.log("user is not verified");
+// // }
 
-console.log(
+// console.log(
 
-     `${isVerified === true ? "User is verified" : "User is not verified" }`
-)
+//      `${isVerified === true ? "User is verified" : "User is not verified" }`
+// )
    
+
+function getTimeString(time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60; 
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+console.log(getTimeString(7865));
