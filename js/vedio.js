67a890d0a6fd0c1ac41bc97b9a2ce09ec188e646
shahadot99999 <shahadot99999@gmail.com>
2025-02-22 22:23:25 +0600

    Render Conditional Data On Dynamic Card

--- js/vedio.js
+++ js/vedio.js
@@ -48,6 +48,8 @@ const loadVidoes = () =>{
     .catch((error)=>console.log(error));
  }
 
+//<img class="w-5" src ="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png ">
+
  const displayVideos = (videos)=>{
 
     const vidoContent = document.getElementById("vedios");
@@ -58,11 +60,12 @@ const loadVidoes = () =>{
         card.classList = "card card-compact "
         card.innerHTML = 
         `
-         <figure class = "h-[200px]">
+         <figure class = "h-[200px] relative">
            <img
              src=${video.thumbnail}
              class = "h-full w-full object-cover"
              alt="Shoes" />
+             <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${video.others.posted_date}</span>
         </figure>
         <div class="px-0 py-2 flex gap-2">
            <div>
@@ -72,7 +75,8 @@ const loadVidoes = () =>{
            <h2 class="font-bold">${video.title}</h2>
            <div class = " flex items-center gap-2">
                <p class="text-gray-400">${video.authors[0].profile_name}</p>
-               <img class="w-5" src ="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png ">
+               ${video.authors[0].verified === true ? `<img class="w-5" src ="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png ">` : ""}
+               
            </div>
            
            <p></p>
---
+++ test.js
@@ -0,0 +1,13 @@
+const isVerified = "";
+
+// if(isVerified == true){
+//     console.log("User is verified");
+// }else{
+//     console.log("user is not verified");
+// }
+
+console.log(
+
+     `${isVerified === true ? "User is verified" : "User is not verified" }`
+)
+   
